3a8aea453da7ea452bb280c0fa75d42cd17f54ab
jacquelineyin <yin.sangxu@gmail.com>
2020-10-29 21:57:24 -0700

    not sure what I changed in navBar

--- src/test/java/webpages/NavBar.java
+++ src/test/java/webpages/NavBar.java
@@ -12,12 +12,24 @@ public class NavBar extends PageObject{
     @FindBy(xpath = "//a[contains(text(), 'Home')]")
     private WebElement home_button;
 
+    @FindBy(xpath = "//a[contains(text(), 'Find owners')]")
+    private WebElement find_owners_button;
+
+    @FindBy(xpath = "//a[contains(text(), 'Veterinarians')")
+    private WebElement veternarians_button;
+
     @FindBy (xpath=".//a[contains(text(), ' Error')]")
     private WebElement error_button;
+
+
 
     public void pressHomeButton() {
         this.home_button.click();
     }
+
+    public void pressFindOwnersButton() { this.find_owners_button.click(); }
+
+    public void pressVeternariansButton() { this.veternarians_button.click(); }
 
     public void pressErrorButton() {
         this.error_button.click();
