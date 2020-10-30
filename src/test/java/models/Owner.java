a15b363f1afa1dde45d3dbff75a8e1dc21b23846
jacquelineyin <yin.sangxu@gmail.com>
2020-10-29 21:55:53 -0700

    Implemented an Owners class for more moduler test data

--- src/test/java/models/Owner.java
+++ src/test/java/models/Owner.java
@@ -1,2 +1,24 @@
-package models;public class Owner {
+package models;
+
+public class Owner {
+    public Owner(String firstName, String lastName,
+                 String address, String city, String telephone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String telephone;
+
+    public String getFirstName(){return firstName;}
+    public String getLastName(){return lastName;}
+    public String getAddress() {return address;}
+    public String getCity() {return city;}
+    public String getTelephone() {return telephone;}
 }
