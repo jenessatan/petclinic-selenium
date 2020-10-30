c2b04f046b4fb06909dd2cd446f817e2bf10e044
jacquelineyin <yin.sangxu@gmail.com>
2020-10-29 21:59:07 -0700

    Implemented navigateTest and searchOwner (non-existent) tests. Implementing searchOwner(existent) test

--- src/test/java/tests/FindOwnersPageTest.java
+++ src/test/java/tests/FindOwnersPageTest.java
@@ -1,2 +1,91 @@
-package tests;public class FindOwnersPageTest {
+package tests;
+
+import models.Owner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import org.testng.annotations.Test;
+import webpages.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class FindOwnersPageTest {
+    private static final WebDriver driver = new ChromeDriver();
+    private static NavBar navBar;
+    private static FindOwnersPage findOwnersPage;
+
+    @BeforeSuite
+    public static void main(String[] args) {
+        // ChromeDriver location set up in webpages.Utils class
+        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+    }
+
+    @BeforeMethod
+    public static void init() {
+        driver.get(Utils.BASE_URL);
+        navBar = new NavBar(driver);
+
+        //Navigate to FindOwners Page
+        navBar.pressFindOwnersButton();
+        findOwnersPage = new FindOwnersPage(driver);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test(enabled = true)
+    public static void navigateToFindOwnersPageTest(){
+        Assert.assertTrue(findOwnersPage.hasCorrectPath());
+
+        Assert.assertTrue(findOwnersPage.hasFindOwnersHeader());
+        Assert.assertTrue(findOwnersPage.hasCorrectSearchForm());
+        Assert.assertTrue(findOwnersPage.hasAddOwnerLink());
+    }
+
+    @Test(enabled = true)
+    public static void searchNonExistentOwnersTest(){
+        findOwnersPage.searchForOwners("@12345");
+        WebElement hasNotBeenFound = driver.findElement(By.id("owner.errors"));
+        Assert.assertTrue(hasNotBeenFound != null);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public static void searchExistentOwnersTest(){
+        Owner owner_robert = new Owner("Robert", "Shaw",
+                "Random Address", "Victoria", "6041231234");
+
+        //add an owner and nav back to findOwners page
+        addOwner(owner_robert);
+        navToFindOwnersPage();
+        //search for newly created owner
+        findOwnersPage.searchForOwners(owner_robert.getLastName());
+
+
+
+    }
+
+    private static void addOwner(Owner owner){
+        findOwnersPage.clickAddOwnerLink();
+        AddOwnerFormPage addOwnerFormPage = new AddOwnerFormPage(driver);
+
+        addOwnerFormPage.createNewOwner(owner.getFirstName(), owner.getLastName(),
+                owner.getAddress(), owner.getCity(), owner.getTelephone());
+    }
+
+    private static void navToFindOwnersPage() {
+        navBar.pressFindOwnersButton();
+        findOwnersPage = new FindOwnersPage(driver);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public static void cleanUp(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
 }
--- src/test/java/webpages/FindOwnersPage.java
+++ src/test/java/webpages/FindOwnersPage.java
@@ -1,4 +1,60 @@
 package webpages;// Page URL: http://localhost:9966/petclinic/owners/find.html
 
-public class FindOwnersPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class FindOwnersPage extends PageObject {
+    public FindOwnersPage(WebDriver driver) {
+        super(driver);
+        search_input = search_form.findElement(By.xpath("//label[contains(text(), 'Last name')]"));
+        search_input = search_form.findElement(By.xpath("//input[@id='lastName']"));
+        submit_button = search_form.findElement(By.xpath("//button[@type='submit']"));
+    }
+
+    private String EXPECTED_PATH = "/petclinic/owners/find.html";
+
+    @FindBy(xpath = "//h2[contains(text(),'Find Owners')]")
+    private WebElement header;
+
+    @FindBy(id = "search-owner-form")
+    private WebElement search_form;
+
+    @FindBy(xpath = "//a[text()='Add Owner']")
+    private WebElement add_owner_link;
+
+    private WebElement search_label;
+    private WebElement search_input;
+    private WebElement submit_button;
+
+    public void searchForOwners(String lastName){
+        search_input.sendKeys(lastName);
+        submit_button.click();
+    };
+
+    public void clickAddOwnerLink() {
+        add_owner_link.click();
+    }
+
+    public Boolean hasFindOwnersHeader() {
+        return header != null;
+    }
+
+    public Boolean hasCorrectPath() {
+        return getPath().equals(EXPECTED_PATH);
+    }
+
+    public Boolean hasCorrectSearchForm(){
+        Boolean isSearchFormCorrect =
+                search_form != null
+                && search_label != null
+                && search_input != null
+                && submit_button != null;
+        return isSearchFormCorrect;
+    }
+
+    public Boolean hasAddOwnerLink() {
+        return add_owner_link != null;
+    }
 }
